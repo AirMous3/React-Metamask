c7fda311f2cc35f5c1533fee7543649b14b4e9b2
AirMous3 <airmouselol@gmail.com>
2022-04-08 13:06:00 +0300

    add ui

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+import {Button} from 'react-bootstrap';
+import metamaskImage from './assets/metamask.svg';
+import disconnectImage from './assets/noun_waving_3666509 .svg';
+import './App.css';
+
 export const App = () => {
-  return <div>123</div>
-}
+    return <div className="App">
+        <header className="App-header">
+            <Button variant="secondary">
+                <img src={metamaskImage} alt="metamask" width={50} height={50}/>
+                Connect to Metamask
+            </Button>
+            <div className="mt-2 mb-2">
+                Connected account:
+            </div>
+            <Button variant="danger">
+                Disconnect Metamask
+                <img src={disconnectImage} alt="disconnectImage" width={50} height={50}/>
+            </Button>
+        </header>
+    </div>;
+};
